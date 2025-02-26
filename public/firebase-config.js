0a828073b9f76f96d75eace7a680a59a41f38963
Korhan Mulcar <korhanmulcar@Korhan-MacBook-Air.local>
2025-02-26 20:01:35 +0400

    Add web app files: login, dashboard, signature, and firebase-config

---
+++ public/firebase-config.js
@@ -0,0 +1,14 @@
+// firebase-config.js
+// This file initializes Firebase using the compat libraries loaded via CDN.
+const firebaseConfig = {
+    apiKey: "AIzaSyBom7DbS972c5Flepx14yGzqivGlnmWYDE",
+    authDomain: "delivery-tracking-81c66.firebaseapp.com",
+    projectId: "delivery-tracking-81c66",
+    storageBucket: "delivery-tracking-81c66.firebasestorage.app",
+    messagingSenderId: "344188937236",
+    appId: "1:344188937236:web:be381760f3b50f661cf301"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
